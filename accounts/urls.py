24f83a99517ae1b64ffe1a88c2f9f38165458a29
ORiVS <107984700+ORiVS@users.noreply.github.com>
2025-04-20 09:55:00 +0200

    Fix: clean vendor registration with signal-based UserProfile creation

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('registerUser/', views.registerUser, name='registerUser'),
-    path('registerVendor/', views.registerVendor, name='registerVendor'),
-    path('login/', views.loginUser, name='loginUser'),
-    path('logout/', views.logoutUser, name='logoutUser'),
+    path('registerVendor/', views.registerVendor, name='registerVendor')
+
 ]

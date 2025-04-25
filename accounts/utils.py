796542728a59ef3ba01f49e801378c12432dc200
ORiVS <107984700+ORiVS@users.noreply.github.com>
2025-04-25 16:16:33 +0200

    feat(auth): ajout de la vérification par code (email ou téléphone) lors de l'inscription

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,13 +1,12 @@
 from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, send_mail
 from django.urls import reverse
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 
-
-
+"""
 def send_verification_email(request, user):
     current_site = get_current_site(request)
     uid = urlsafe_base64_encode(force_bytes(user.pk))
@@ -25,3 +24,14 @@ def send_verification_email(request, user):
     email = EmailMessage(mail_subject, message, to=[to_email])
     email.content_subtype = 'html'
     email.send()
+
+"""
+def send_verification_code_email(user):
+    subject = "Votre code de vérification"
+    message = f"Bonjour {user.first_name},\n\nVoici votre code : {user.verification_code}\n\nL'équipe LeMaillot"
+    send_mail(subject, message, 'no-reply@lemaillot.com', [user.email])
+    print(f"[EMAIL] Code envoyé à {user.email} : {user.verification_code}")
+
+def send_verification_code_sms(user):
+    # Simulation, à remplacer par un vrai service (ex : Twilio)
+    print(f"SMS to {user.phone_number}: Code = {user.verification_code}")

fc47894f70b1762326d0d499f07d439c5d95b8de
ORiVS <107984700+ORiVS@users.noreply.github.com>
2025-07-10 01:53:10 +0200

    add seting logging

--- leMaillotApi/settings.py
+++ leMaillotApi/settings.py
@@ -179,3 +179,17 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
